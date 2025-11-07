d3b825aaf5874bc4b0e4abdb6a55545fc95d1fac
duanyhui <783809979@qq.com>
2025-11-07 11:44:45 +0800

    refactor download scripts and update configuration files for clarity and resource management

--- script/_download_assets.sh
+++ script/_download_assets.sh
@@ -2,16 +2,16 @@ cd assets
 python _download.py
 
 # background_texture
-unzip background_texture.zip
-rm -rf background_texture.zip
+#unzip background_texture.zip
+#rm -rf background_texture.zip
 
 # embodiments
 unzip embodiments.zip
 rm -rf embodiments.zip
 
 # objects
-unzip objects.zip
-rm -rf objects.zip
+#unzip objects.zip
+#rm -rf objects.zip
 
 cd ..
 echo "Configuring Path ..."
